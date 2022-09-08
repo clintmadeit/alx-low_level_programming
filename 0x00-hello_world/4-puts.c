5faf17e219b81702d05e1447027c7c69289b20f3
clintmadeit <clintonbrirdley@gmail.com>
2022-09-08 21:59:41 +0000

    my first c code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *Main - entry
+ *
+ *Return:0 after printing function 
+ */
+init main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+	Return (0);
+}
