c5b6ccd2581fd21d43467b90396d0ca16dec8b93
clintmadeit <clintonbrirdley@gmail.com>
2022-09-09 13:15:49 +0000

    print without printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main - Entery point
+ *
+ * Return: Always 1(success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" -Dora Korpar, 2015-10-19\n");
+	return (1);
+}
