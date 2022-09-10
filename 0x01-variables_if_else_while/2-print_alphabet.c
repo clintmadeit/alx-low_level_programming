0970e00cd819d6d84965fd9a3f0af0f163d181fa
clintmadeit <clintonbrirdley@gmail.com>
2022-09-10 21:10:44 +0000

    alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,23 +1,21 @@
 #include <stdio.h>
-/*
- * Main - Entry point: prints alphabets in lower case to the screen
- *
- * Description: "a function that puts alphabetic characters on screen"
+/**
+ * main - entry point
+ * Description:using the main function
+ * this program "prints alphabets in order of assending"
  *
  * Return: Always 0
- *
  */
 int main(void)
 {
-	char ch;
-	{
-	for (ch = 'a'; ch <= 'z'; ch++)
+char ch;
 
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
 	putchar(ch);
-	}
-
-	{
+}
+{
 	putchar('\n');
-	}
-	return (0);
+}
+return (0);
 }
