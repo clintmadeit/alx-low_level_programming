e40ff4d6670c0f0f9f28df1a1d45dcfdcf7a0b05
clintmadeit <clintonbrirdley@gmail.com>
2022-09-10 21:58:53 +0000

    while loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * program description
+ * function that "prints all digits of base 10 from 0"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+int n;
+n = 0;
+while (n < 10)
+	n++;
+
+{
+	printf("%d\n", n);
+}
+
+return (0);
+}
