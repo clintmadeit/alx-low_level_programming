4b3013a3531c1ae7a44617fca08c99e73f16271a
clintmadeit <clintonbrirdley@gmail.com>
2022-09-10 09:52:31 +0000

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,15 +5,16 @@
  * This program executes the putchar function and returns a true output
  * Return:0 (success)
  */
-int main(void)
+char main(void)
 {
-char ch;
-while (ch = 'a' && ch = < 'z')
-ch++;
+	char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 {
 	putchar(ch);
 }
-putchar("\n")
+{
+putchar("\n");
+}
 return (0);
 }
 
