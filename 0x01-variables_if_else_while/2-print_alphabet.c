7ab1e4f28f431d19cba63949dce39d9fdafb421a
clintmadeit <clintonbrirdley@gmail.com>
2022-09-10 08:43:34 +0000

    putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * This program executes the putchar function and returns a true output
+ * Return:0 (success)
+ */
+int main(void)
+{
+char ch;
+while (ch = 'a' && ch = < 'z')
+ch++;
+{
+	putchar(ch);
+}
+putchar("\n")
+return (0);
+}
+
