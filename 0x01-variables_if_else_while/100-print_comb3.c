080c8f9da09388b48ba10c82bd0e93a7fe6a30e1
clintmadeit <clintonbrirdley@gmail.com>
2022-09-11 18:05:21 +0000

    2digit numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,23 +8,32 @@
  */
 int main(void)
 {
-	int i;
+	int d1, d2;
 
-	for (i = 01; i <= 99; i++)
+	for (d1 = '0' ; d1 <= '9' ; d1++)
+	{
+		for (d2 = '1' ; d2 <= '9' ; d2++)
+		{
+			if (d1 != d2 && d1 < d2)
+			{
+				putchar(d1);
 
-	if (!(i = 10 || 11 || 22 || 33 || 44 || 55 || 66 || 77 || 88 || 99))
+				putchar(d2);
 
-	{	putchar(i);
+				if (!(d1 == '9' && d2 == '8'))
 
-		if (i != 98)
+				{	putchar(',');
 
-		{	putchar(',');
+					putchar(' ');
+				}
 
-			putchar(' ');
+			}
 
 		}
-	}
-		putchar('\n');
 
-		return (0);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
