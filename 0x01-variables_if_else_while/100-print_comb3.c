1765e50476a2c2f0ef26afe35feb6c9c8649a57b
clintmadeit <clintonbrirdley@gmail.com>
2022-09-11 13:46:59 +0000

    2 digit numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * programs description
+ * function that "prints all possible 2 digit number combination"
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 01; i <= 99; i++)
+
+	if (!(i <= 10 || 11 || 22 || 33 || 44 || 55 || 66 || 77 || 88 || 99))
+
+	{	putchar(i);
+
+		if (i != 98)
+
+		{	putchar(',');
+
+			putchar(' ');
+
+		}
+	}
+		putchar('\n');
+
+		return (0);
+}
