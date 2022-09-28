835d8cc3180f592465323d7f5b8ef96722d04c8f
clintmadeit <clintonbrirdley@gmail.com>
2022-09-28 20:36:53 +0000

    C-Recursions and factorials

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,29 +1,72 @@
-/*                                                                                                                                    
- * File: 6-is_prime_number.c                                                                                                          
- * Auth: sam tech                                                                                                                     
- */                                                                                                                                   
-                                                                                                                                      
-#include "main.h"                                                                                                                     
-                                                                                                                                      
-int is_divisible(int num, int div);                                                                                                   
-int is_prime_number(int n);                                                                                                           
-                                                                                                                                      
-/**                                                                                                                                   
- * is_divisible - Checks if a number is divisible.                                                                                    
- * @num: The number to be checked.                                                                                                    
- * @div: The divisor.                                                                                                                 
- *                                                                                                                                    
- * Return: If the number is divisible - 0.                                                                                            
- *         If the number is not divisible - 1.                                                                                        
- */                                                                                                                                   
-int is_divisible(int num, int div)                                                                                                    
-{                                                                                                                                     
-        if (num % div == 0)                                                                                                           
-                return (0);                                                                                                           
-                                                                                                                                      
-        if (div == num / 2)                                                                                                           
-                return (1);                                                                                                           
-                                                                                                                                      
-        return (is_divisible(num, div + 1));                                                                                          
+#include "main.h"
+
+/**
+* prime2 - Makes possible to evaluate from 1 to n
+* @a: same number as n
+* @b: number that iterates from 1 to n
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+**/
+
+int prime2(int a, int b)
+
+{
+
+	if (a == b)
+
+		return (1);
+
+	else if (a % b == 0)
+
+		return (0);
+
+	return (prime2(a, b + 1));
 }
 
+/**
+* is_prime_number - checks if a number is prime
+* @n: Number Integer
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+**/
+
+int is_prime_number(int n)
+
+{
+
+	if (n <= 1)
+
+		return (0);
+
+	return (prime2(n, 2));
+}
+
+Main.h
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+
+void _print_rev_recursion(char *s);
+
+
+int _strlen_recursion(char *s);
+
+
+int factorial(int n);
+
+
+int _pow_recursion(int x, int y);
+
+
+int _sqrt_recursion(int n);
+
+
+int is_prime_number(int n);
+
+
+int is_palindrome(char *s);
+
+
+int wildcmp(char *s1, char *s2);
