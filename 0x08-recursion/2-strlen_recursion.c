f07c2600ad33e0cf20a47359bc835693678f439b
clintmadeit <clintonbrirdley@gmail.com>
2022-09-28 20:15:34 +0000

    C-Recursions and factorials

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,26 +1,23 @@
-/*                                                                                                                                      
- * File: 2-strlen_recursion.c                                                                                                           
- * Auth: smone                                                                                                                          
- */                                                                                                                                     
-                                                                                                                                        
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * _strlen_recursion - Returns the length of a string.                                                                                  
- * @s: The string to be measured.                                                                                                       
- *                                                                                                                                      
- * Return: The length of the string.                                                                                                    
- */                                                                                                                                     
-int _strlen_recursion(char *s)                                                                                                          
-{                                                                                                                                       
-        int len = 0;                                                                                                                    
-                                                                                                                                        
-        if (*s)                                                                                                                         
-        {                                                                                                                               
-                len++;                                                                                                                  
-                len += _strlen_recursion(s + 1);                                                                                        
-        }                                                                                                                               
-                                                                                                                                        
-        return (len);                                                                                                                   
-}
+#include "main.h"
 
+/**
+* _strlen_recursion - Returns Length of String
+* @s: string
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+**/
+
+int _strlen_recursion(char *s)
+
+{
+
+	if (*s != '\0')
+
+	{
+
+		return (1 + _strlen_recursion(s + 1));
+
+	}
+
+	return (0);
+}
