1dc63776d49ec47a482c8ad83bdfa17e3ae79bd3
clintmadeit <clintonbrirdley@gmail.com>
2022-09-28 20:05:42 +0000

    C-Recursions and factorials

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,14 +1,14 @@
-/*                                                                                                                                      
+/**                                                                                                                                      
  * File: 1-print_rev_recursion.c                                                                                                        
  * Auth: sam                                                                                                                            
- */                                                                                                                                     
+ **/                                                                                                                                     
                                                                                                                                         
 #include "main.h"                                                                                                                       
                                                                                                                                         
 /**                                                                                                                                     
  * _print_rev_recursion - Prints a string in reverse.                                                                                   
  * @s: The string to be printed.                                                                                                        
- */                                                                                                                                     
+ **/                                                                                                                                     
 void _print_rev_recursion(char *s)                                                                                                      
 {                                                                                                                                       
         if (*s)                                                                                                                         
@@ -17,4 +17,3 @@ void _print_rev_recursion(char *s)
                 _putchar(*s);                                                                                                           
         }                                                                                                                               
 }
-
