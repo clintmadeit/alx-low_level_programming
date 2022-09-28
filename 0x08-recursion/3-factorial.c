4c5962c9d6ac5bdfebcdb16a9207cd42345a78b0
clintmadeit <clintonbrirdley@gmail.com>
2022-09-28 20:18:39 +0000

    C-Recursions and factorials

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,29 +1,23 @@
-/*                                                                                                                                      
- * 3-factorial.c                                                                                                                        
- * Auth: sam                                                                                                                            
- */                                                                                                                                     
-                                                                                                                                        
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * factorial - Returns the factorial of a given number.                                                                                 
- * @n: The number to find the factorial of.                                                                                             
- *                                                                                                                                      
- * Return: If n > 0 - the factorial of n.                                                                                               
- *         If n < 0 - 1 to indicate an error.                                                                                           
- */                                                                                                                                     
-int factorial(int n)                                                                                                                    
-{                                                                                                                                       
-        int result = n;                                                                                                                 
-                                                                                                                                        
-        if (n < 0)                                                                                                                      
-                return (-1);                                                                                                            
-                                                                                                                                        
-        else if (n >= 0 && n <= 1)                                                                                                      
-                return (1);                                                                                                             
-                                                                                                                                        
-        result *= factorial(n - 1);                                                                                                     
-                                                                                                                                        
-        return (result);                                                                                                                
-}
+#include "main.h"
 
+/**
+* factorial - gets factorial of n
+* @n: integer
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+**/
+
+int factorial(int n)
+
+{
+
+	if (n < 0)
+
+		return (-1);
+
+	if (n == 0)
+
+		return (1);
+
+	return (n * factorial(n - 1));
+}
