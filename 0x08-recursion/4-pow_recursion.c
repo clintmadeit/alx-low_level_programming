92a38200fb7808b6473a9bf5230c144e234349f2
clintmadeit <clintonbrirdley@gmail.com>
2022-09-29 20:07:59 +0000

    C-Recursions

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,29 +1,18 @@
-/**                                                                                                                                      
- * File: 4-pow_recursion.c                                                                                                              
- * Auth: sam                                                                                                                            
- **/                                                                                                                                     
-                                                                                                                                        
-#include "main.h"                                                                                                                       
-                                                                                                                                        
-/**                                                                                                                                     
- * _pow_recursion - Returns the value of x raised to the power of y.                                                                    
- * @x: The number to be raised.                                                                                                         
- * @y: The power.                                                                                                                       
- *                                                                                                                                      
- * Return: The value of x raised to the power of y.                                                                                     
- **/
+#include "main.h"
 
-int _pow_recursion(int x, int y)                                                                                                        
-{                                                                                                                                       
-        int result = x;                                                                                                                 
-                                                                                                                                        
-        if (y < 0)                                                                                                                      
-                return (-1);                                                                                                            
-                                                                                                                                        
-        else if (y == 0)                                                                                                                
-                return (1);                                                                                                             
-                                                                                                                                        
-        result *= _pow_recursion(x, y - 1);                                                                                             
-                                                                                                                                        
-        return (result);                                                                                                                
+/**
+ *	_pow_recursion -> power using recursion
+ * @x: x ^ y
+ * @y: the power number
+ * Return: power result 
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+
 }
