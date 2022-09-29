18ce257fe0eb1b9e98a3141eb6f7193f20d47a7d
clintmadeit <clintonbrirdley@gmail.com>
2022-09-29 20:24:29 +0000

    C-Recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,15 +3,15 @@
 /**
  *	_pow_recursion -> power using recursion
  * @x: x ^ y
- * @y: the power number
+ * @y:the power number
  * Return: power result 
  */
 
 int _pow_recursion(int x, int y)
 {
-	if(y < 0)
+	if (y < 0)
 		return (-1);
-	if(y == 0)
+	if (y == 0)
 		return (1);
 	return (x * _pow_recursion(x, y - 1));
 }
